985817aad0fc58f637901c159f2019fbbbc7387d
yang <yang@yang-t500>
2013-05-16 23:21:24 +0800

    baike crawler

---
+++ servercode/crawler/baidubaike/crawl_baike_in_one_page.js
@@ -0,0 +1,187 @@
+var baikeList = [ 'http://wapbaike.baidu.com/view/667852.htm', 'http://wapbaike.baidu.com/view/25276.htm', 'http://wapbaike.baidu.com/view/293208.htm', 'http://wapbaike.baidu.com/view/53288.htm',
+        'http://wapbaike.baidu.com/view/21620.htm', 'http://wapbaike.baidu.com/view/2132269.htm', 'http://wapbaike.baidu.com/view/2497107.htm', 'http://wapbaike.baidu.com/view/1372114.htm',
+        'http://wapbaike.baidu.com/view/2776793.htm', 'http://wapbaike.baidu.com/view/44942.htm', 'http://wapbaike.baidu.com/view/4056607.htm', 'http://wapbaike.baidu.com/view/1874345.htm',
+        'http://wapbaike.baidu.com/view/97968.htm', 'http://wapbaike.baidu.com/view/548346.htm', 'http://wapbaike.baidu.com/view/6201658.htm', 'http://wapbaike.baidu.com/view/120365.htm',
+        'http://wapbaike.baidu.com/view/3715630.htm', 'http://wapbaike.baidu.com/view/4267184.htm', 'http://wapbaike.baidu.com/view/2002858.htm', 'http://wapbaike.baidu.com/view/1003513.htm',
+        'http://wapbaike.baidu.com/view/1043523.htm', 'http://wapbaike.baidu.com/view/2881721.htm', 'http://wapbaike.baidu.com/view/3415063.htm', 'http://wapbaike.baidu.com/view/1863071.htm',
+        'http://wapbaike.baidu.com/view/1751582.htm', 'http://wapbaike.baidu.com/view/4003065.htm', 'http://wapbaike.baidu.com/view/2195554.htm', 'http://wapbaike.baidu.com/view/617921.htm',
+        'http://wapbaike.baidu.com/view/3588933.htm', 'http://wapbaike.baidu.com/view/1818025.htm', 'http://wapbaike.baidu.com/view/1980944.htm', 'http://wapbaike.baidu.com/view/2708552.htm',
+        'http://wapbaike.baidu.com/view/6271061.htm', 'http://wapbaike.baidu.com/view/1021963.htm', 'http://wapbaike.baidu.com/view/2856954.htm', 'http://wapbaike.baidu.com/view/1589815.htm',
+        'http://wapbaike.baidu.com/view/3287965.htm', 'http://wapbaike.baidu.com/view/4151366.htm', 'http://wapbaike.baidu.com/view/1333603.htm', 'http://wapbaike.baidu.com/view/3959105.htm',
+        'http://wapbaike.baidu.com/view/1019908.htm', 'http://wapbaike.baidu.com/view/723907.htm', 'http://wapbaike.baidu.com/view/5903474.htm', 'http://wapbaike.baidu.com/view/3014767.htm',
+        'http://wapbaike.baidu.com/view/1588171.htm', 'http://wapbaike.baidu.com/view/1000222.htm', 'http://wapbaike.baidu.com/view/126007.htm', 'http://wapbaike.baidu.com/view/1238087.htm',
+        'http://wapbaike.baidu.com/view/5669480.htm', 'http://wapbaike.baidu.com/view/1772127.htm', 'http://wapbaike.baidu.com/view/5127678.htm', 'http://wapbaike.baidu.com/view/3859079.htm',
+        'http://wapbaike.baidu.com/view/444248.htm', 'http://wapbaike.baidu.com/view/2100972.htm', 'http://wapbaike.baidu.com/view/2298927.htm', 'http://wapbaike.baidu.com/view/4576404.htm',
+        'http://wapbaike.baidu.com/view/6326873.htm', 'http://wapbaike.baidu.com/view/4532552.htm', 'http://wapbaike.baidu.com/view/222677.htm', 'http://wapbaike.baidu.com/view/2539490.htm',
+        'http://wapbaike.baidu.com/view/2947651.htm', 'http://wapbaike.baidu.com/view/2581244.htm', 'http://wapbaike.baidu.com/view/3330427.htm', 'http://wapbaike.baidu.com/view/5289143.htm',
+        'http://wapbaike.baidu.com/view/348955.htm', 'http://wapbaike.baidu.com/view/2675570.htm', 'http://wapbaike.baidu.com/view/1297262.htm', 'http://wapbaike.baidu.com/view/2132654.htm',
+        'http://wapbaike.baidu.com/view/3227782.htm', 'http://wapbaike.baidu.com/view/262426.htm', 'http://wapbaike.baidu.com/view/1145936.htm', 'http://wapbaike.baidu.com/view/3393108.htm',
+        'http://wapbaike.baidu.com/view/609739.htm', 'http://wapbaike.baidu.com/view/3689204.htm', 'http://wapbaike.baidu.com/view/237730.htm', 'http://wapbaike.baidu.com/view/6214990.htm',
+        'http://wapbaike.baidu.com/view/6375144.htm', 'http://wapbaike.baidu.com/view/5862902.htm', 'http://wapbaike.baidu.com/view/1518794.htm', 'http://wapbaike.baidu.com/view/6334857.htm',
+        'http://wapbaike.baidu.com/view/1014920.htm', 'http://wapbaike.baidu.com/view/998593.htm', 'http://wapbaike.baidu.com/view/2135365.htm', 'http://wapbaike.baidu.com/view/6987686.htm',
+        'http://wapbaike.baidu.com/view/3923098.htm', 'http://wapbaike.baidu.com/view/3223912.htm', 'http://wapbaike.baidu.com/view/3319930.htm', 'http://wapbaike.baidu.com/view/562423.htm',
+        'http://wapbaike.baidu.com/view/1174760.htm', 'http://wapbaike.baidu.com/view/2632507.htm', 'http://wapbaike.baidu.com/view/1839958.htm', 'http://wapbaike.baidu.com/view/3229957.htm',
+        'http://wapbaike.baidu.com/view/3295642.htm', 'http://wapbaike.baidu.com/view/6657813.htm', 'http://wapbaike.baidu.com/view/1140316.htm', 'http://wapbaike.baidu.com/view/1496351.htm',
+        'http://wapbaike.baidu.com/view/5394305.htm', 'http://wapbaike.baidu.com/view/3113551.htm', 'http://wapbaike.baidu.com/view/6295392.htm', 'http://wapbaike.baidu.com/view/6250069.htm',
+        'http://wapbaike.baidu.com/view/2729237.htm', 'http://wapbaike.baidu.com/view/5580492.htm', 'http://wapbaike.baidu.com/view/6290427.htm', 'http://wapbaike.baidu.com/view/5127029.htm',
+        'http://wapbaike.baidu.com/view/1280725.htm', 'http://wapbaike.baidu.com/view/1702716.htm', 'http://wapbaike.baidu.com/view/1001958.htm', 'http://wapbaike.baidu.com/view/3176332.htm',
+        'http://wapbaike.baidu.com/view/2057111.htm', 'http://wapbaike.baidu.com/view/3203981.htm', 'http://wapbaike.baidu.com/view/5930469.htm', 'http://wapbaike.baidu.com/view/2118853.htm',
+        'http://wapbaike.baidu.com/view/5894791.htm', 'http://wapbaike.baidu.com/view/1477755.htm', 'http://wapbaike.baidu.com/view/1043513.htm', 'http://wapbaike.baidu.com/view/3623991.htm',
+        'http://wapbaike.baidu.com/view/6175243.htm', 'http://wapbaike.baidu.com/view/3230494.htm', 'http://wapbaike.baidu.com/view/1483258.htm', 'http://wapbaike.baidu.com/view/2197507.htm',
+        'http://wapbaike.baidu.com/view/1800053.htm', 'http://wapbaike.baidu.com/view/2074460.htm', 'http://wapbaike.baidu.com/view/2177482.htm', 'http://wapbaike.baidu.com/view/2074486.htm',
+        'http://wapbaike.baidu.com/view/2766733.htm', 'http://wapbaike.baidu.com/view/1588163.htm', 'http://wapbaike.baidu.com/view/1642570.htm', 'http://wapbaike.baidu.com/view/7099316.htm',
+        'http://wapbaike.baidu.com/view/2905469.htm', 'http://wapbaike.baidu.com/view/3259925.htm', 'http://wapbaike.baidu.com/view/1740063.htm', 'http://wapbaike.baidu.com/view/1699391.htm',
+        'http://wapbaike.baidu.com/view/1490548.htm', 'http://wapbaike.baidu.com/view/5697308.htm', 'http://wapbaike.baidu.com/view/5930567.htm', 'http://wapbaike.baidu.com/view/1060453.htm',
+        'http://wapbaike.baidu.com/view/6291623.htm', 'http://wapbaike.baidu.com/view/6319166.htm', 'http://wapbaike.baidu.com/view/7223642.htm', 'http://wapbaike.baidu.com/view/5335196.htm',
+        'http://wapbaike.baidu.com/view/5156933.htm', 'http://wapbaike.baidu.com/view/6278855.htm', 'http://wapbaike.baidu.com/view/7671945.htm', 'http://wapbaike.baidu.com/view/8681308.htm',
+        'http://wapbaike.baidu.com/view/6308816.htm', 'http://wapbaike.baidu.com/view/2662729.htm', 'http://wapbaike.baidu.com/view/4890525.htm', 'http://wapbaike.baidu.com/view/2730484.htm',
+        'http://wapbaike.baidu.com/view/2352159.htm', 'http://wapbaike.baidu.com/view/4292541.htm', 'http://wapbaike.baidu.com/view/6270524.htm', 'http://wapbaike.baidu.com/view/4782697.htm',
+        'http://wapbaike.baidu.com/view/7875229.htm', 'http://wapbaike.baidu.com/view/5388973.htm', 'http://wapbaike.baidu.com/view/2755084.htm', 'http://wapbaike.baidu.com/view/8336704.htm',
+        'http://wapbaike.baidu.com/view/8153555.htm', 'http://wapbaike.baidu.com/view/4584889.htm', 'http://wapbaike.baidu.com/view/1079445.htm', 'http://wapbaike.baidu.com/view/6622494.htm',
+        'http://wapbaike.baidu.com/view/1661389.htm', 'http://wapbaike.baidu.com/view/548227.htm', 'http://wapbaike.baidu.com/view/5903542.htm', 'http://wapbaike.baidu.com/view/807668.htm',
+        'http://wapbaike.baidu.com/view/3034290.htm', 'http://wapbaike.baidu.com/view/1024383.htm', 'http://wapbaike.baidu.com/view/3454095.htm', 'http://wapbaike.baidu.com/view/4277848.htm',
+        'http://wapbaike.baidu.com/view/4971036.htm', 'http://wapbaike.baidu.com/view/2941310.htm', 'http://wapbaike.baidu.com/view/5126423.htm', 'http://wapbaike.baidu.com/view/5126404.htm',
+        'http://wapbaike.baidu.com/view/6791539.htm', 'http://wapbaike.baidu.com/view/5126695.htm', 'http://wapbaike.baidu.com/view/5799136.htm', 'http://wapbaike.baidu.com/view/1153022.htm',
+        'http://wapbaike.baidu.com/view/44222.htm', 'http://wapbaike.baidu.com/view/5903732.htm', 'http://wapbaike.baidu.com/view/5903516.htm', 'http://wapbaike.baidu.com/view/2876898.htm',
+        'http://wapbaike.baidu.com/view/447273.htm', 'http://wapbaike.baidu.com/view/5683390.htm', 'http://wapbaike.baidu.com/view/5127563.htm', 'http://wapbaike.baidu.com/view/8278895.htm',
+        'http://wapbaike.baidu.com/view/3230011.htm', 'http://wapbaike.baidu.com/view/3186479.htm', 'http://wapbaike.baidu.com/view/3571979.htm', 'http://wapbaike.baidu.com/view/6474892.htm',
+        'http://wapbaike.baidu.com/view/1297172.htm', 'http://wapbaike.baidu.com/view/793880.htm', 'http://wapbaike.baidu.com/view/1025798.htm', 'http://wapbaike.baidu.com/view/3074316.htm' ];
+
+
+var rootPath = '/y/git/H2/H2/servercode/tmp/';
+
+var fs = require('fs');
+var cs = require('casper').create({
+    pageSettings : {
+        loadImages : true,
+        loadPlugins : true,
+        userAgent : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.91 Safari/537.11'
+    },
+    viewportSize : {
+        width : 800,
+        height : 600
+    },
+    clientScripts : [ 'jquery-1.9.1.js' ]
+});
+
+var html = [];
+var header = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/></head><body>";
+var footer = "</body><html>";
+var globalIndex = 0;
+
+cs.start();
+loadHtml(baikeList[globalIndex], globalIndex++);
+
+function loadHtml(url, n) {
+    var htmlPath = rootPath + n + '.html';
+    var imagePath = rootPath + n + '.jpg';
+    cs.thenOpen(url);
+    cs.then(function() {
+        //cs.capture(imagePath);
+        var info = this.evaluate(doJob);
+        console.log(info);
+        html[html.length] = info.content;
+        if (info.next) {
+            console.log(info.next);
+            loadHtml(info.next, n);
+        } else {
+            fs.write(htmlPath, header + html.join('\n') + footer, 'w');
+            if (globalIndex < baikeList.length) {
+                //console.log("##### Start loading " + url + ",  storing at " + htmlPath);
+                html = [];
+                loadHtml(baikeList[globalIndex], globalIndex++);
+            } else {
+                console.log('All done');
+            }
+        }
+    });
+}
+
+cs.run();
+function doJob() {
+    try {
+        var r = {
+            next : null,
+            content : null
+        };
+        // get next page link
+
+        var link = document.evaluate('//a[text()="下页"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        if (link && link.singleNodeValue && link.singleNodeValue.href) {
+            r.next = link.singleNodeValue.href;
+        } else {
+            r.next = null;
+        }
+
+        // replace image with base 64 encoded src
+        function getBase64Image(img) {
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext("2d");
+            console.log(1);
+            ctx.drawImage(img, 0, 0);
+            var dataURL = canvas.toDataURL("image/png");
+            return dataURL;
+            return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+        }
+
+        var images = document.getElementsByTagName('img');
+        for ( var i in images) {
+            if (images[i] && images[i].src)
+                images[i].src = getBase64Image(images[i]);
+        }
+
+        // remove A tag
+        var links = document.getElementsByTagName('a');
+        for ( var i in links) {
+            links[i].href = null;
+        }
+
+        // remove references
+        var t = document.getElementsByClassName('rec-con');
+        if (t && t.length != 0) {
+            for ( var i in t) {
+                if (t[i] && t[i].parentNode)
+                    t[i].parentNode.removeChild(t[i]);
+            }
+        }
+
+        // remove alumbs
+        var t = document.getElementsByClassName('album');
+        if (t && t.length != 0) {
+            for ( var i = t.length - 1; i > -1; i--) {
+                var n = t[i];
+                console.log(n);
+                while (n) {
+                    var c = n.nextSibling;
+                    // console.log('remove');
+                    // console.log(n);
+                    if (n.parentNode) {
+                        n.parentNode.removeChild(n);
+                    }
+                    n = c;
+                }
+            }
+        }
+
+        // get content html
+        var main = document.getElementById('main');
+        if (main) {
+            var header = main.getElementsByClassName('polysemy_tip');
+            var footer = main.getElementsByClassName('foot-tool');
+            if (header && header.length > 0) {
+                main.removeChild(header[0]);
+            }
+            if (footer && footer.length > 0) {
+                main.removeChild(footer[0]);
+            }
+            r.content = main.innerHTML;
+        } else {
+            r.content = "[none html]";
+        }
+        return r;
+    } catch (e) {
+        return e.toString();
+    }
+}
