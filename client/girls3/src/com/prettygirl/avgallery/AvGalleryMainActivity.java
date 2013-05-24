1a3b21e2efe52bb7a44be5e3bad029b21cefc05c
jiangliu1212 <jiangliu1212@gmail.com>
2013-05-24 12:05:20 +0800

    changes

--- client/girls3/src/com/prettygirl/avgallery/AvGalleryMainActivity.java
+++ client/girls3/src/com/prettygirl/avgallery/AvGalleryMainActivity.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
@@ -17,6 +20,9 @@ import android.widget.SectionIndexer;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.prettygirl.avgallery.components.FixedTabsView;
+import com.prettygirl.avgallery.components.TabsAdapter;
+import com.prettygirl.avgallery.components.ViewPagerTabButton;
 import com.prettygirl.avgallery.dialog.ExitDialog;
 import com.prettygirl.avgallery.model.AVGirl;
 import com.prettygirl.avgallery1.R;
@@ -29,27 +35,44 @@ public class AvGalleryMainActivity extends MBaseActivity {
 
     private ArrayList<AVGirl> mGirls;
 
+    private FixedTabsView mFixedTabsView;
+
+    private ViewPager mViewPager;
+
+    private String[] tabs;
+
+    private View[] views = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.av_gallery_main_activity);
-
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.av_gallery_main_cust_title_with_tab);
+        
         mLang = AvApplication.getCurrentLang();
         mGirls = ((AvApplication) getApplicationContext()).getGirlList(mLang);
 
-        GridView mGridView = (GridView) findViewById(R.id.av_gallery_grid_view);
-        //        mGridView.setFastScrollEnabled(true);
-        mGridView.setAdapter(new AVAdapter());
-        mGridView.setOnItemClickListener(new OnItemClickListener() {
+        tabs = getResources().getStringArray(R.array.av_detail_tabs);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mFixedTabsView = (FixedTabsView) findViewById(R.id.fixedTabsView);
+        mViewPager.setAdapter(mPageAdapter);
+        mFixedTabsView.setAdapter(mTabsAdapter);
+        mFixedTabsView.setViewPager(mViewPager);
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent();
-                intent.setClass(parent.getContext(), AvGalleryDetailActivity.class);
-                intent.putExtra(EXT_KEY_GIRL, mGirls.get(position));
-                startActivity(intent);
-            }
-        });
+        //        GridView mGridView = (GridView) findViewById(R.id.av_gallery_grid_view);
+        //        //        mGridView.setFastScrollEnabled(true);
+        //        mGridView.setAdapter(new AVAdapter());
+        //        mGridView.setOnItemClickListener(new OnItemClickListener() {
+        //
+        //            @Override
+        //            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //                Intent intent = new Intent();
+        //                intent.setClass(parent.getContext(), AvGalleryDetailActivity.class);
+        //                intent.putExtra(EXT_KEY_GIRL, mGirls.get(position));
+        //                startActivity(intent);
+        //            }
+        //        });
     }
 
     @Override
@@ -146,5 +169,79 @@ public class AvGalleryMainActivity extends MBaseActivity {
         }
 
     }
+
+    TabsAdapter mTabsAdapter = new TabsAdapter() {
+
+        @Override
+        public View getView(int position) {
+            if (tabs == null) {
+                return null;
+            } else {
+                View result = View.inflate(getBaseContext(), R.layout.av_gallery_detail_tab, null);
+                ViewPagerTabButton text = (ViewPagerTabButton) result.findViewById(R.id.av_detail_tab_text);
+                text.setText(getResources().getIdentifier("av_personal_" + tabs[position], "string", getPackageName()));
+                return result;
+            }
+        }
+
+    };
+
+    PagerAdapter mPageAdapter = new PagerAdapter() {
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            int index = position % tabs.length;
+            if (views == null) {
+                views = new View[tabs.length];
+            }
+            if (views[index] == null) {
+                views[index] = View.inflate(AvGalleryMainActivity.this, R.layout.av_gallery_main_activity_tabs_content, null);
+            } else {
+                container.removeView(views[index]);
+            }
+            if ("info".equals(tabs[position])) {
+                views[index].findViewById(R.id.av_gallery_news).setVisibility(View.GONE);
+                views[index].findViewById(R.id.av_gallery_grid_view).setVisibility(View.VISIBLE);
+                GridView mGridView = (GridView) views[index].findViewById(R.id.av_gallery_grid_view);
+                mGridView.setAdapter(new AVAdapter());
+                mGridView.setOnItemClickListener(new OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent();
+                        intent.setClass(parent.getContext(), AvGalleryDetailActivity.class);
+                        intent.putExtra(EXT_KEY_GIRL, mGirls.get(position));
+                        startActivity(intent);
+                    }
+                });
+            } else if ("news".equals(tabs[position])) {
+                views[index].findViewById(R.id.av_gallery_news).setVisibility(View.VISIBLE);
+                views[index].findViewById(R.id.av_gallery_grid_view).setVisibility(View.GONE);
+                //                ((WebView) views[index].findViewById(R.id.av_gallery_main_news)).loadUrl(ServerUtils
+                //                        .getPicServerRoot(AvGalleryMainActivity.this) + "/jp/wordpress");
+            }
+            ((ViewPager) container).addView(views[index], 0);
+            return views[index];
+        }
+
+        @Override
+        public int getCount() {
+            if (tabs == null) {
+                return 0;
+            } else {
+                return tabs.length;
+            }
+        }
+    };
 
 }
